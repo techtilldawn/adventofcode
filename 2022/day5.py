0063521f0d0ed869ae5c88235cdc5bb73c474419
x3mTox <michael.biro@outlook.de>
2023-01-05 13:10:03 +0100

    add part 2

--- 2022/day5.py
+++ 2022/day5.py
@@ -19,18 +19,23 @@ def main():
                         obj[i].reverse()
                     firsttime = False
                 cmdlist = row.strip().split(" ")
-                obj = move_container(obj, cmdlist[3], cmdlist[5], int(cmdlist[1]))
+                obj = crate_mover_9001(obj, cmdlist[3], cmdlist[5], int(cmdlist[1]))
                 
     for i in range(1, len(obj)+1):
         print(obj[str(i)][-1], end="")
     print()
 
 
-def move_container(obj, f, t, amt):
+def crate_mover_9000(obj, f, t, amt):
     for i in range(amt): 
         obj[t].append(obj[f][-1])
         obj[f].pop()
     return obj
 
+def crate_mover_9001(obj, f, t, amt):
+    for i in range(-amt, 0, 1): 
+        obj[t].append(obj[f][i])
+        obj[f].pop(i)
+    return obj
 
 main()
