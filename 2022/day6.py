6db24a05c1e03a2919772dce67e0d9a57b0ce56a
x3mTox <michael.biro@outlook.de>
2023-01-05 14:24:10 +0100

    day6 part2

--- 2022/day6.py
+++ 2022/day6.py
@@ -3,7 +3,10 @@ def main():
     with open("/home/michael/tmp/input_d6.txt", "r") as input:
         for i in input:
             com = i.strip()
-    
+    print(part1(com))
+    print(part2(com))
+
+def part1(com):
     for n in range(len(com)):
         tmplist = []
         if n >= len(com) - 3:
@@ -14,6 +17,19 @@ def main():
 
         if len(lset) == 4:
             print(f"set: {lset}, n: {n}")
+            return n + 4
+
+def part2(com):
+    for n in range(len(com)):
+        tmplist = []
+        if n >= len(com) - 13:
             break
+        for m in range(14):
+            tmplist.append(com[n+m])
+        lset = set(tmplist)
+
+        if len(lset) == 14:
+            print(f"set: {lset}, n: {n}")
+            return n + 14
         
 main()
